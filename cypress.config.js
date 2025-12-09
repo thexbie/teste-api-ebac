e62d72db66dafcbf831df9fba486d7050efd9c10
Bie <thexbie@gmail.com>
2025-12-09 13:38:00 -0300

    alteração baseURL

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://serverest.dev",
+    baseUrl: "http://localhost:3000/",
   },
 });
